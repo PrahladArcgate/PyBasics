0f8b1d2c561d3e032d479afe4767dd8352919582
prahlad <prahlad@arcgate.com>
2022-06-22 17:57:09 +0530

    Day 22june

---
+++ Tuple.py
@@ -0,0 +1,18 @@
+thistuple =("apple","banana","cherry")
+
+print(thistuple)
+print(type(thistuple))
+
+#Dictionary 
+
+d= {1:'value' ,'key':2} # Example !.
+print(type(d))
+
+
+d1= {
+    'course_id' :'python',
+    'course_bookname ' : 'java',
+    'course_Duration ': '2 months'
+}
+print(d1)
+print(type(d1))
