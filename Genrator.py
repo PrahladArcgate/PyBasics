36160f404f88ff3d386c56349fc97ab513a8a348
prahlad <prahlad@arcgate.com>
2022-07-01 09:54:26 +0530

    30 june advancePythonEOD

---
+++ Genrator.py
@@ -0,0 +1,31 @@
+'''
+-iterable
+-iterator
+-itration
+
+'''
+
+
+
+def gen(n):                                        #object
+    for i in range():
+      yield i
+  
+# print(gen(100000))
+# for i in range(100):
+#     print(i)
+
+num1 = "Arcgate"
+#for i in (num1):
+
+iter1= iter(num1)
+print(next(iter1))
+print(next(iter1))
+print(next(iter1))
+print(next(iter1))
+print(next(iter1))
+print(next(iter1))
+print(next(iter1))
+# print(next(iter1))
+# print(next(iter1))
+ 
