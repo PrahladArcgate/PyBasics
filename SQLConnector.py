aae2fd7faf633c9145206e06e15245680195a07e
prahlad <prahlad@arcgate.com>
2022-07-06 12:09:31 +0530

    SQL Cheetsheet with config

---
+++ SQLConnector.py
@@ -0,0 +1,9 @@
+
+import mysql.connector as c
+con=c.connect(host="localhost", user="prahlad",passwd="Pra@1234",database="learn")
+# if con.is_connected():
+#     print("successfully connected...")
+#     con.commit()                                       For checking
+# else:
+#     print("Data is not valid")
+
